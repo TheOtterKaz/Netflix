c36d6a4ec70e49a93e31d39a726600b7184390db
TheOtterKaz <mae.grenier.01@edu.cegeptr.qc.ca>
2023-10-27 12:03:54 -0400

    Commitage

--- app/Models/Personne.php
+++ app/Models/Personne.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Personne extends Model
 {
     use HasFactory;
+
+    // Fonction films réalisés
+    public function filmsRealises()
+    {
+        return $this->hasMany(Film::class, 'realisateur_id');
+    }
+
+    // Fonction films produits
+    public function filmsProduits()
+    {
+        return $this->hasMany(Film::class, 'producteur_id');
+    }
 }
