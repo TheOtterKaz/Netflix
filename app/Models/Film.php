a61d46a333e160c26a583e09dab40c9361bd7fe2
TheOtterKaz <mae.grenier.01@edu.cegeptr.qc.ca>
2023-11-03 10:36:45 -0400

    recomitage

--- app/Models/Film.php
+++ app/Models/Film.php
@@ -8,6 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Film extends Model
 {
     use HasFactory;
+    protected $fillable = ['titre', 'resume', 'brand', 'collection', 'cote', 'rating', 
+    'duree', 'annee', 'realisateur_id', 'producteur_id', 'type', 'imageFilm', 'lienFilm'];
 
     // Fonction realisateur
     public function realisateur()
